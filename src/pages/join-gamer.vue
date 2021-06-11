dc7edacd5c9a249b29d725828ba5a1ea2fe9f8d6
samira.n <samira.n@marcopacs.com>
2021-06-11 21:47:19 +0430

    uuid

--- src/pages/join-gamer.vue
+++ src/pages/join-gamer.vue
@@ -48,6 +48,27 @@
 </template>
 <script>
 import axios from "axios";
+import {v4 as uuidv4} from 'uuid';
+const v4options = {
+  random: [
+    0x10,
+    0x91,
+    0x56,
+    0xbe,
+    0xc4,
+    0xfb,
+    0xc1,
+    0xea,
+    0x71,
+    0xb4,
+    0xef,
+    0xe1,
+    0x67,
+    0x1c,
+    0x58,
+    0x36,
+  ],
+};
 export default {
     data(){
 		return{
@@ -65,6 +86,9 @@ export default {
 			this.$router.back()
 		},
 		joinToGame(){
+			// generate unified id = uuid
+			let gamerUuid= uuidv4([Math.floor(Math.random()*100),Date.now()]);
+
 			axios({
 				method:'POST',
 				url:`http://localhost:8000/api/gamers/`,
